1aa9233fc0450e4c3c824978d1c599ba73016ea5
Till Wehowski <github@webfan.de>
2020-06-11 10:41:16 +0200

    Create OIDLookUpRpcMethod.php

---
+++ src/OIDLookUpRpcMethod.php
@@ -0,0 +1,33 @@
+<?php
+namespace frdlweb\Api\Whois\OID;
+
+use frdlweb\Api\Rpc\MethodDiscoverableInterface;
+use frdlweb\Api\Rpc\Server;
+use stdClass;
+
+
+use frdlweb\Api\Rpc\DiscoverMethod;
+
+class OIDLookUpRpcMethod extends DiscoverMethod implements MethodDiscoverableInterface
+{
+
+
+ 
+public function getSpec(): ?\stdClass
+    {
+        return \json_decode('
+{
+ "$schema": "https://look-up.webfan3.de/plugins/publicPages/095_attachments/download.php?id=oid:1.3.6.1.4.1.37553.8.1.8.1.13878.31765&filename=oid-look-up.json#",
+  "type": ["null", "array", "object"],
+  "properties": {
+  },
+  "required" : [],
+  "additionalProperties": true
+}
+'
+        );
+
+}
+    
+    
+}
